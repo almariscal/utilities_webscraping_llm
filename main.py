88445288ef222616c95870fd92f09631bbe4fa6b
almariscal <almariscal93@gmail.com>
2024-12-09 17:02:44 +0100

    Uncomment PDF generation

--- main.py
+++ main.py
@@ -12,17 +12,17 @@ load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 # Converting the websites to PDF
-#from PDFGenerator import PDFGenerator
-#import asyncio
+from PDFGenerator import PDFGenerator
+import asyncio
 
 # Ruta del archivo con las URLs
-#websites_file = "websites.txt"
+websites_file = "websites.txt"
 
 # Crear una instancia de PDFGenerator
-#pdf_generator = PDFGenerator(websites_file)
+pdf_generator = PDFGenerator(websites_file)
 
 # Ejecutar el proceso para generar los PDFs
-#asyncio.run(pdf_generator.process_all_websites())
+asyncio.run(pdf_generator.process_all_websites())
 
 import os
 import shutil
